295a04685553b9eb63bf441b043d6ac07662e749
IngridLemos <ingridlc42@gmail.com>
2025-07-30 12:02:32 -0300

    ajustando a ft_strlcat.c para passar nos teste

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 12:03:17 by ilemos-c          #+#    #+#             */
-/*   Updated: 2025/07/30 10:54:12 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/07/30 11:54:39 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,31 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
 	size_t	dst_length;
 	size_t	src_length;
 
 	i = 0;
-	j = 0;
 	dst_length = 0;
 	src_length = 0;
-	while (dst[dst_length])
+	while (dst[dst_length] && dst_length < size)
 		dst_length++;
 	while (src[src_length])
 		src_length++;
-	if (size == 0)
+	if (dst_length == size)
 		return (size + src_length);
-	i = dst_length;
-	while (src[j] && (i < size - 1))
+	while (src[i] && (dst_length + i + 1 < size))
 	{
-		dst[i] = src[j];
+		dst[dst_length + i] = src[i];
 		i++;
-		j++;
 	}
-	dst[i] = '\0';
+	if (dst_length + i < size)
+		dst[dst_length + i] = '\0';
 	return (dst_length + src_length);
 }
 /*
 int main(void)
 {
-	char dst[10] = "Hello ";
+	char dst[20] = "Hello";
 	const char *src = "World";
 	size_t size = ft_strlcat(dst, src, sizeof(dst));
 	printf("dst: %s\n", dst);
