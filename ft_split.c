e74ed63e7ef8170727e3c1c2a8548dd6741fdbef
IngridLemos <ingridlc42@gmail.com>
2025-07-31 17:09:24 -0300

    create main for function ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 11:12:45 by ingrid            #+#    #+#             */
-/*   Updated: 2025/07/31 16:27:14 by ilemos-c         ###   ########.fr       */
+/*   Updated: 2025/07/31 17:06:20 by ilemos-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,10 @@ char	**ft_split(char const *s, char c)
 	unsigned int	j;
 	unsigned int	start;
 	char			**words;
-	unsigned int	count_words;
 
 	i = 0;
 	j = 0;
-	count_words = ft_count_words(s, c);
-	words = malloc(sizeof(char *) * (count_words + 1));
+	words = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
 	if (!words)
 		return (NULL);
 	while (s[i])
@@ -66,11 +64,14 @@ char	**ft_split(char const *s, char c)
 /*
 int	main(void)
 {
-	//const char	s[100] = "-Hello--Ingrid-a";
-	char	c = '-';
-	//printf("%d\n", ft_count_words(s, c));
-	const char	s[100] = "-Hello--Ingrid-a";
+	const char	s[100] = "-Hello--Ingrid-a--";
+	char	c = ' ';
 	char	**ptr = ft_split(s, c);
-	printf("%s", ptr[2]);
+	int	i = 0;
+	while (ptr[i])
+	{
+		printf("%s\n", ptr[i]);
+		i++;
+	}
 	return (0);
 }*/
