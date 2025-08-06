fd74011f059a4802196f3ce878400ce47eb41b3c
IngridLemos <ingridlc42@gmail.com>
2025-08-06 11:03:31 -0300

    create function back

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,8 +6,24 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:51:17 by ingrid            #+#    #+#             */
-/*   Updated: 2025/08/06 10:51:30 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/08/06 11:03:09 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*add_back;
+
+	if (lst)
+	{
+		if (*lst)
+		{
+			add_back = ft_lstadd_back(*lst);
+			add_back->next = new;
+		}
+		else
+			*lst = new;
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 11:28:23 by ilemos-c          #+#    #+#             */
-/*   Updated: 2025/08/06 10:36:20 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/08/06 10:52:42 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
