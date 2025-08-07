413178ae97dbfcc53aca2a60304a09dd468ab6e6
IngridLemos <ingridlc42@gmail.com>
2025-08-07 10:05:57 -0300

    create test for ft_lstlast

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:35:51 by ingrid            #+#    #+#             */
-/*   Updated: 2025/08/06 10:42:23 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/08/07 10:05:33 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,30 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (lst);
 }
+/*
+int	main (void)
+{
+	t_list *head = NULL;
+
+	ft_lstadd_front(&head, ft_lstnew("A"));
+	ft_lstadd_front(&head, ft_lstnew("B"));
+	ft_lstadd_front(&head, ft_lstnew("C"));
+
+	t_list *temp = head;
+
+	while (temp)
+	{
+		printf("[%s] -> ", (char *)temp->content);
+		temp = temp->next;
+	}
+	printf("NULL\n");
+
+	t_list	*last = ft_lstlast(head);
+
+	if(last)
+		printf("Último nó: [%s]\n", (char *)last->content);
+	else
+		printf("A Lista está vazia\n");
+
+	return (0);
+}*/
