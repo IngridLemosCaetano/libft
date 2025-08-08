d50093913f50ada8e345c3372c3b02c66f859489
IngridLemos <ingridlc42@gmail.com>
2025-08-08 10:29:35 -0300

    create exemplo for ft_lstdelone

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 14:57:04 by ilemos-c          #+#    #+#             */
-/*   Updated: 2025/08/07 23:16:34 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/08/08 10:20:13 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,24 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 		(del)(lst->content);
 	free(lst);
 }
+/*
+void	ft_delete_content(void	*content)
+{
+	printf("Deletando: %s\n", (char *)content);
+	free(content);
+}
 
 int	main(void)
 {
-	t_list	*head = NULL;
+	t_list	*node = malloc(sizeof(t_list));
+	node->content = ft_strdup("Bom Dia!");
+	node->next = NULL;
 
-	int	*a = malloc(sizeof(int));
-	int	*b = malloc(sizeof(int));
-	int	*c = malloc(sizeof(int));
+	printf("Nó criado com conteúdo: %s\n", (char *)node->content);
 
+	ft_lstdelone(node, ft_delete_content);
+
+	printf("Nó deletado com sucesso!\n");
 
 	return (0);
-}
+}*/
