67ebcf882bc259f67148e5f1bf0228fad6b35a4f
IngridLemos <ingridlc42@gmail.com>
2025-08-08 20:58:47 -0300

    refactor ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,53 +6,80 @@
 /*   By: ingrid <ingrid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 17:14:27 by ilemos-c          #+#    #+#             */
-/*   Updated: 2025/08/08 19:42:57 by ingrid           ###   ########.fr       */
+/*   Updated: 2025/08/08 20:54:54 by ingrid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	get_length(long n)
+{
+	int	len;
+
+	len = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*allocate_string(int len, int is_negative)
+{
+	char	*n;
+
+	n = malloc (sizeof(char) * (len + is_negative + 1));
+	if (!n)
+		return (NULL);
+	return (n);
+}
+
+void	fill_digits(char *str, long n, int index)
+{
+	if (n == 0)
+	{
+		str[index] = '0';
+		return ;
+	}
+	while (n > 0)
+	{
+		str[index] = (n % 10) + '0';
+		n /= 10;
+		index--;
+	}
+}
+
 char	*ft_itoa(int n)
 {
-	int		is_negative;
-	int		len_n;
-	long	nbr;
-	long	temp;
 	char	*ptr;
-	int		index;
+	int		is_negative;
+	long	nbr;
+	int		len;
 
-	len_n = 1;
-	nbr = (long)n;
 	is_negative = 0;
-	if (nbr < 0)
+	nbr = (long)n;
+	if (n < 0)
 	{
 		is_negative = 1;
 		nbr = -nbr;
 	}
-	temp = nbr;
-	while (temp / 10 > 0)
-	{
-		temp = temp / 10;
-		len_n++;
-	}
-	ptr = malloc(sizeof(char) * (len_n + is_negative + 1));
+	len = get_length(nbr);
+	ptr = allocate_string(len, is_negative);
 	if (!ptr)
 		return (NULL);
-	index = len_n + is_negative - 1;
-	while (index >= is_negative)
-	{
-		ptr[index--] = (nbr % 10) + '0';
-		nbr /= 10;
-	}
+	fill_digits(ptr, nbr, len + is_negative - 1);
 	if (is_negative)
 		ptr[0] = '-';
-	ptr[len_n + is_negative] = '\0';
+	ptr[len + is_negative] = '\0';
 	return (ptr);
 }
 /*
 int	main(void)
 {
 	int	n = -42;
-	printf("%s\n", ft_itoa(n));
+	char	*ptr = ft_itoa(n);
+	printf("%s\n", ptr);
+	free (ptr);
 	return (0);
 }*/
